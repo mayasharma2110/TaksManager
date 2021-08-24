a809797e0cb8a00f845b2ee9b3f06a5c1c2f484d
Maya Sharma <mayasharma95@outlook.com>
2021-08-24 12:46:19 +0000

    minor fixes

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world...again"
+    return "Hello World ... again!"
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
-        port=int(os.environ.get("PORT")),
-        debug=True)
+            port=int(os.environ.get("PORT")),
+            debug=True)
